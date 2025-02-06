382f076e61567e56893923924f4f891993c95544
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-06 09:35:35 +0100

    Add Footer component with styling and copyright notice

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer style={styles.footer}>
+			<p>My first React app, all rights reserved &copy; 2021</p>
+		</footer>
+	);
+}
+
+const styles = {
+	footer: {
+		backgroundColor: "black",
+		color: "white",
+		textAlign: "center",
+		padding: "10px",
+	},
+};
+
+export default Footer;
