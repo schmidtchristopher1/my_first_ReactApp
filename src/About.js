e1a6451dbf6e41a6689d25b8ee5dfafd7e3f2b6b
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-06 09:35:21 +0100

    Add About component with Header and Footer

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function About() {
+	return (
+		<div>
+			<Header name='About' />
+			<h1>About</h1>
+			<p>This is the about page</p>
+			<Footer />
+		</div>
+	);
+}
+
+export default About;
