59d0d894448bb7d59da9836950e0b1db39354221
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-10 11:56:17 +0100

    Implement interactive TO-DO list in Home component with task addition functionality

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function Home() {
-	const tasks = [
-		"Meet John",
-		"Buy groceries",
-		"Clean the house",
-		"Cook dinner",
-		"Walk the dog",
-	];
+    // const tasks = [
+    //     "Meet John",
+    //     "Buy groceries",
+    //     "Clean the house",
+    //     "Cook dinner",
+    //     "Walk the dog",
+    // ];
 
-	function renderList() {
-		return tasks.map((task, index) => <li key={index}>{task}</li>);
-	}
+    const [task, setTask] = useState([]);
+    const [inputTask, setInputTask] = useState('');
 
-	return (
-		<div>
-			<Header name='Home' />
-			<h1>TO-DO List</h1>
-			<ul>{renderList()}</ul>
-			<Footer />
-		</div>
-	);
+    function addTask() {
+        if (!inputTask) return;
+        setTask([...task, inputTask]);
+        setInputTask('');
+    }
+
+    function renderList() {
+        return (
+            <div>
+                <ul>
+                    {task.map((task, index) => (
+                        <li key={index}>{task}</li>
+                    ))}
+                </ul>
+                <input type="text" value={inputTask} onChange={(e) => setInputTask(e.target.value)}
+                       placeholder="Set new task"/>
+                <button onClick={addTask}>
+                    Add task
+                </button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Header name='Home'/>
+            <h1 style={styles.h1}>TO-DO List</h1>
+            <ul style={styles.ul}>{renderList()}</ul>
+            <Footer/>
+        </div>
+    );
+}
+
+const styles = {
+    ul: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        listStyle: "none",
+        padding: "0",
+    },
+    h1: {
+        textAlign: "center",
+    }
 }
 
 export default Home;
