11abec36e56d955e01422159c65a81476d027af3
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-10 11:56:11 +0100

    Add styling to About component for improved text alignment

--- src/About.js
+++ src/About.js
@@ -3,14 +3,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function About() {
-	return (
-		<div>
-			<Header name='About' />
-			<h1>About</h1>
-			<p>This is the about page</p>
-			<Footer />
-		</div>
-	);
+    return (
+        <div>
+            <Header name='About'/>
+            <h1 style={styles.h1}>About</h1>
+            <p style={styles.p}>This is the about page</p>
+            <Footer/>
+        </div>
+    );
+}
+
+const styles = {
+    h1: {
+        textAlign: "center",
+    },
+    p: {
+        textAlign: "center",
+    },
 }
 
 export default About;
